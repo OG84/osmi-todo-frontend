9efceb19434573f843a5d6a34b23fa224ae01501
Christian Götze <c.goetze@gmx.eu>
2018-04-22 15:21:26 +0200

    add ngrx for reactive programming

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Params, RouterStateSnapshot } from '@angular/router';
 import { TodoListsComponent } from './todo-lists/todo-lists.component';
 import { HomeComponent } from './home/home.component';
+import { RouterReducerState, RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
 
 const routes: Routes = [
   {
@@ -10,8 +11,43 @@ const routes: Routes = [
   }
 ];
 
+export interface RouterStateUrl {
+  url: string;
+  params: Params;
+  queryParams: Params;
+}
+
+export interface State {
+  router: RouterReducerState<RouterStateUrl>;
+}
+
+export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    let route = routerState.root;
+
+    while (route.firstChild) {
+      route = route.firstChild;
+    }
+
+    const { url, root: { queryParams } } = routerState;
+    const { params } = route;
+
+    // Only return an object including the URL, params and query params
+    // instead of the entire snapshot
+    return { url, params, queryParams };
+  }
+}
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router'
+    }),
+  ],
+  exports: [RouterModule],
+  providers: [
+    { provide: RouterStateSerializer, useClass: CustomSerializer }
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatButtonModule, MatSidenavModule, MatIconModule, MatMenuModule, MatListModule, MatToolbarModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatMenuModule,
+  MatListModule,
+  MatToolbarModule
+} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TodoListsComponent } from './todo-lists/todo-lists.component';
 import { HomeComponent } from './home/home.component';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { StoreModule, MetaReducer, ActionReducerMap } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { AppState } from 'src/app/app-state.model';
+
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer
+};
 
 @NgModule({
   declarations: [
@@ -23,7 +41,12 @@ import { HomeComponent } from './home/home.component';
     MatIconModule,
     MatMenuModule,
     MatListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
