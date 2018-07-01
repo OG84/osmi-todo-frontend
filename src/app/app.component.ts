73c073b1ed80cee89213bfff07162621e832b048
Christian Götze <c.goetze@gmx.eu>
2018-07-01 16:26:07 +0200

    implement qr code to continue working on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ObservableMedia } from '@angular/flex-layout';
 import { ToolbarService } from './toolbar/toolbar.service';
 import { Observable } from 'rxjs';
 import { MatSidenav } from '@angular/material';
 import { routerTransition } from 'src/app/router-transistion.animation';
+import * as qrcode from 'qrcode';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'osmi-todo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [ routerTransition ]
+  animations: [routerTransition]
 })
 export class AppComponent implements OnInit {
 
@@ -19,10 +21,12 @@ export class AppComponent implements OnInit {
   isMobile = false;
   sidenavMode = 'side';
   toolbarTitle: Observable<string>;
+  qrCodeSvg: SafeResourceUrl;
 
   constructor(
     public readonly media: ObservableMedia,
-    private readonly toolbarService: ToolbarService) {
+    private readonly toolbarService: ToolbarService,
+    private readonly domSanitizer: DomSanitizer) {
 
   }
 
@@ -32,6 +36,21 @@ export class AppComponent implements OnInit {
     this.media.subscribe(x => {
       this.isMobile = x.mqAlias === 'xs' || x.mqAlias === 'sm';
       this.sidenavMode = this.isMobile ? 'over' : 'side';
+    });
+
+    const options = {
+      color: {
+        dark: '#3f51b548',
+        light: '#fafafa'
+      }
+    };
+
+    qrcode.toString(window.location.href, options, (err, svgString) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      this.qrCodeSvg = this.domSanitizer.bypassSecurityTrustResourceUrl('data:image/svg+xml;charset=utf-8,' + svgString);
     });
   }
 
